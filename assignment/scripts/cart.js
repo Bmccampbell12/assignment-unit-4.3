9419a698af6f207919d6e84a2c8a14f1d2c67244
Bruce McCampbell <mccampbellbruce1@gmail.com>
2024-08-25 14:18:39 -0500

    assignment 4.3 functions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,14 +1,34 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+
+function addItem(item) {
+    basket.push(item);
+    return true;
+}
+function listItems(item) {
+    for (let i = 0; i < basket.length; i++) {
+console.log(basket[i]);
+    }
+
+}
+function empty(){
+ basket.splice(0, basket.length);
+}
 
 
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+//Stretch Goals:
 
+let maxItems = 5;
+function isFull(){
 
+}
 
-
-
-
+console.log(7);
 
 
 
